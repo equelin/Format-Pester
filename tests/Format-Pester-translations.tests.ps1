0b620c6c2b57e0a8a61ea661ee56ed9877d949be
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-08-02 23:06:49 +0200

    0.2.0 - 2016-08-02 - Checking if null/empty strings are used, messages for failed tests improved

--- tests/Format-Pester-translations.tests.ps1
+++ tests/Format-Pester-translations.tests.ps1
@@ -1,16 +1,52 @@
-Import-Module Pester
+<#
+    .SYNOPSIS
+    Pester tests to validate translations of PowerShell psd1 files.
+
+    .DESCRIPTION
+    Pester test to validate completeness, version and equality between main language (en-US) and other languages stored in language related subfolders.
+
+    The tests created initially for Format-Pester project.
+
+    .LINK
+    https://github.com/equelin/Format-Pester
+
+    .NOTES
+    AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
+	KEYWORDS: PowerShell, Translation, Pester
+   
+	VERSIONS HISTORY
+    0.1.0 - 2016-07-30 - The first version 
+	0.2.0 - 2016-08-02 - Checking if null/empty strings are used, messages for failed tests improved
+
+	TODO
+    - generalize names used in the code, assign names of modules, paths and file names to variables
+    - improve performance for testing in context "Check translation for $SubfolderInPublicName - detailed fields name comparison" - based on the previously calculated difference
+
+	LICENSE
+    Copyright (c) 2016 Wojciech Sciesinski
+    This function is licensed under The MIT License (MIT)
+    Full license text: https://opensource.org/licenses/MIT
+
+#>
 
 Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
     
+    BeforeAll {
+        
+        Remove-Module -Name Format-Pester -ErrorAction SilentlyContinue
+        
+        Import-Module "$PSScriptRoot\..\Format-Pester" -Force -Scope Global -ErrorAction Stop
+        
+    }
     
     AfterAll {
         
-        #RemoveGlobalVariables
+        #Remove previously defined variables scoped as global
         
         $VariablesToRemove = @("enUSLocalizedStrings1", "enUSLocalizedStrings2", "GlobalenUSLocalizedStrings", "GlobalenUSLocalizedStringCount", "GlobalenUSLocalizedStringKeys", `
-            "SkipDueEnUSNotImported", "GlobalCurrentLanguageLocalizedStringCount", "GlobalCurrentLanguageLocalizedStringKeys")
+            "SkipDueEnUSNotImported", "GlobalCurrentLanguageLocalizedStringCount", "GlobalCurrentLanguageLocalizedStringKeys", "DifferencesInKeys", "KeysNotEqual")
         
-        foreach ($VariableToRemove in $VariablesToRemove) {
+        ForEach ($VariableToRemove in $VariablesToRemove) {
             
             Remove-Variable -Scope Global -Name $VariableToRemove -Force -ErrorAction SilentlyContinue
             
@@ -19,16 +55,12 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
     }
     
     
-    Import-Module "$PSScriptRoot\..\Format-Pester" -Force -Scope Global
-    
     $FormatPesterModule = $(Get-Module -Name Format-Pester)
     
     $FormatPesterModuleVersion = $([Version]$FormatPesterModule.Version).ToString()
     
     $FormatPesterModulePath = $([System.IO.FileInfo]$FormatPesterModule.Path).DirectoryName
-    
-    Write-host $FormatPesterModulePath
-    
+        
     Context 'Compare versions numbers included in module of Format-Pester module and function' {
         
         It 'Compare versions numbers between module and function' {
@@ -39,7 +71,7 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
         
     }
     
-    Context 'Check subfolder en-US - main language' {
+    Context 'Check the subfolder en-US - main language' {
         
         $enUSfolderInPublic = Get-Item -Path "$FormatPesterModulePath\Public\en-US" -ErrorAction SilentlyContinue
         
@@ -58,11 +90,15 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
             
             Import-LocalizedData -BaseDirectory $("$FormatPesterModulePath\Public") -FileName 'Format-Pester.psd1' -BindingVariable enUSLocalizedStrings2 -UICulture 'en-US'
             
-            New-Variable -Scope Global -Name GlobalenUSLocalizedStrings -Value $enUSLocalizedStrings2 -Force
+            New-Variable -Scope Global -Name GlobalenUSLocalizedStrings -Value $enUSLocalizedStrings2
             
             New-Variable -Scope Global -Name GlobalenUSLocalizedStringCount -Value $enUSLocalizedStrings2.Count -Force
             
             New-Variable -Scope Global -Name GlobalenUSLocalizedStringKeys -Value $enUSLocalizedStrings2.Keys -Force
+            
+            $GlobalenUSLocalizedStrings = $GlobalenUSLocalizedStrings.GetEnumerator() | Sort-Object -Property Name
+                        
+            $GlobalenUSLocalizedStringKeys = $GlobalenUSLocalizedStringKeys | Sort-Object
             
         }
         
@@ -78,10 +114,26 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
             
         }
         
+        #Enumerate hashtable and check if keys has assigned values (translations strings are fullfilled)
+        $GlobalenUSLocalizedStrings.GetEnumerator() | ForEach-Object -Process {
+            
+            $CurrentenUSLocalizedStringValue = $_
+            
+            If ([String]::IsNullOrEmpty($CurrentenUSLocalizedStringValue.Value)) {
+                                                
+                It "Check if value for $($_.Key) for en-US is not null or empty." {
+                    
+                    $CurrentenUSLocalizedStringValue.Value | Should Not BeNullOrEmpty
+                    
+                }                
+                
+            }            
+            
+        }
+        
     }
     
-    
-    $SubfoldersInPublic =   hildItem -Path "$FormatPesterModulePath\Public\" -Directory
+    $SubfoldersInPublic = Get-ChildItem -Path "$FormatPesterModulePath\Public\" -Directory
     
     ForEach ($SubfolerInPublic in $SubfoldersInPublic) {
         
@@ -90,7 +142,6 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
         If ($SubfolderInPublicName -ne 'en-US') {
             
             Context "Check translations for subfolder $SubfolderInPublicName" {
-                
                 
                 $SubfolderInPublicNamePath = $SubfolderInPublic.FullName
                 
@@ -116,6 +167,10 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
                     
                     New-Variable -Scope Global -Name GlobalCurrentLanguageLocalizedStringKeys -Value $CurrentLanguageLocalizedStrings2.Keys -Force
                     
+                    $GlobalCurrentLanguageLocalizedStrings = $GlobalCurrentLanguageLocalizedStrings.GetEnumerator() | Sort-Object -Property Name
+                    
+                    $GlobalCurrentLanguageLocalizedStringKeys = $GlobalCurrentLanguageLocalizedStringKeys | Sort-Object
+                    
                 }
                 
                 It "Check if data from the file Format-Pester.psd1 for $SubfolderInPublicName was correctly assigned to variable."  {
@@ -136,21 +191,16 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
                     
                 }
                 
-                It "Compare if names of localization keys for en-US and $SubfolderInPublicName are equal" {
+                #Enumerate hashtable and check if keys has assigned values (translations strings are fullfilled)
+                $GlobalCurrentLanguageLocalizedStrings.GetEnumerator() | ForEach-Object -Process {
                     
-                    $GlobalenUSLocalizedStringKeys | Should be $GlobalCurrentLanguageLocalizedStringKeys
+                    $CurrentCurrentLanguageLocalizedStringValue = $_
                     
-                    New-Variable -Name KeysNotEqual -Value $($GlobalenUSLocalizedStringKeys -ne $GlobalCurrentLanguageLocalizedStringKeys) -Scope Global -Force
-                    
-                }
-                
-                foreach ($CurrentenUSLocalizedKey in $GlobalenUSLocalizedStringKeys) {
-                    
-                    if ($GlobalCurrentLanguageLocalizedStringKeys -notcontains $CurrentenUSLocalizedKey) {
+                    If ([String]::IsNullOrEmpty($CurrentCurrentLanguageLocalizedStringValue.Value)) {
                         
-                        It "The language file $SubfolderInPublicName not contains $CurrentenUSLocalizedKey" {
+                        It "Check if value for $($_.Key) for en-US is not null or empty." {
                             
-                            $GlobalCurrentLanguageLocalizedStringKeys | Should Contain $CurrentenUSLocalizedKey
+                            $CurrentCurrentLanguageLocalizedStringValue.Value | Should Not BeNullOrEmpty
                             
                         }
                         
@@ -158,13 +208,55 @@ Describe -Name 'Unit tests for Format-Pester translations' -Tag 'Translations' {
                     
                 }
                 
-                foreach ($CurrentenLanguageLocalizedKey in $GlobalCurrentLanguageLocalizedStringKeys) {
+                It "Compare if names of localization keys for en-US and $SubfolderInPublicName are equal" {
                     
-                    if ($GlobalenUSLocalizedStringKeys -notcontains $CurrentenLanguageLocalizedKey) {
+                    $DifferencesInKeys = Compare-Object -ReferenceObject $GlobalenUSLocalizedStringKeys -DifferenceObject $GlobalCurrentLanguageLocalizedStringKeys
+                    
+                    If ($DifferencesInKeys) {
                         
-                        It "The language file en-US doesn't contain $CurrentenLanguageLocalizedKey available in $SubfolderInPublicName" {
+                        New-Variable -Name KeysNotEqual -Value $true -Scope Global -Force
+                        
+                        #New-Variable -Name GlobalDifferencesInKeys -Value $DifferencesInKeys -Scope Global -Force
+                        
+                    }
+                    
+                    $KeysNotEqual | Should BeNullOrEmpty
+                    
+                }
+                
+            }
+            
+            If ($KeysNotEqual) {
+                
+                Context "Check translation for $SubfolderInPublicName - detailed fields name comparison" {
+                    
+                    ForEach ($CurrentenUSLocalizedKey in $GlobalenUSLocalizedStringKeys) {
+                        
+                        if ($GlobalCurrentLanguageLocalizedStringKeys -notcontains $CurrentenUSLocalizedKey) {
                             
-                            $GlobalenUSLocalizedStringKeys | Should Contain $CurrentenLanguageLocalizedKey
+                            It "The language file $SubfolderInPublicName not contains $CurrentenUSLocalizedKey" {
+                                
+                                $CurrentLanguageContainResult = ($GlobalCurrentLanguageLocalizedStringKeys -contains $CurrentenUSLocalizedKey)
+                                
+                                $CurrentLanguageContainResult | Should be $true
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    ForEach ($CurrentenLanguageLocalizedKey in $GlobalCurrentLanguageLocalizedStringKeys) {
+                        
+                        if ($GlobalenUSLocalizedStringKeys -notcontains $CurrentenLanguageLocalizedKey) {
+                            
+                            It "The language file en-US doesn't contain $CurrentenLanguageLocalizedKey available in $SubfolderInPublicName" {
+                                
+                                $EnUSLanguageContainResult = ($GlobalenUSLocalizedStringKeys -contains $CurrentenLanguageLocalizedKey)
+                                
+                                $EnUSLanguageContainResult | Should be $true
+                                
+                            }
                             
                         }
                         
