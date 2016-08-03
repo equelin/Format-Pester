edef48a203d1222086c65299814a4be130040288
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-08-03 23:20:36 +0200

    tabs chars replaced with 4 spaces

--- tests/Format-Pester-translations.tests.ps1
+++ tests/Format-Pester-translations.tests.ps1
@@ -12,17 +12,17 @@
 
     .NOTES
     AUTHOR: Wojciech Sciesinski, wojciech[at]sciesinski[dot]net
-	KEYWORDS: PowerShell, Translation, Pester
+    KEYWORDS: PowerShell, Translation, Pester
    
-	VERSIONS HISTORY
+    VERSIONS HISTORY
     0.1.0 - 2016-07-30 - The first version 
-	0.2.0 - 2016-08-02 - Checking if null/empty strings are used, messages for failed tests improved
+    0.2.0 - 2016-08-02 - Checking if null/empty strings are used, messages for failed tests improved
 
-	TODO
+    TODO
     - generalize names used in the code, assign names of modules, paths and file names to variables
     - improve performance for testing in context "Check translation for $SubfolderInPublicName - detailed fields name comparison" - based on the previously calculated difference
 
-	LICENSE
+    LICENSE
     Copyright (c) 2016 Wojciech Sciesinski
     This function is licensed under The MIT License (MIT)
     Full license text: https://opensource.org/licenses/MIT
